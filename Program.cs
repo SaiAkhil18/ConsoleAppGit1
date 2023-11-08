897929265c513df0777c731bc207bb90d4df7b0f
Akhil <saiakhil2310@gmail.com>
2023-11-08 15:00:37 +0530

    git iniated

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "C:\\Akhil\\file.txt";
+            FileStream f1 = new FileStream(filename, FileMode.OpenOrCreate);
+            using (StreamWriter s1 = new StreamWriter(f1))
+            {
+                s1.WriteLine("Hello, how are you?");
+            }
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
